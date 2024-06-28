17dec523e5a8d54693b16a39dc973fec54d50950
Patternscompany <pandureddypatterns@gmail.com>
2024-06-28 13:21:59 +0530

    pp

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 
 
 const app = express();
-app.use(express.static(path.join(__dirname, 'public')));   // Set the directory containing your templates
+app.use(express.static(path.join(__dirname, 'public')));  
+ // Set the directory containing your templates
+
+ const uploadsDir = path.join(__dirname, 'uploads');
+ if (!fs.existsSync(uploadsDir)){
+     fs.mkdirSync(uploadsDir);
+ }
 
 // Body parser middleware to parse form data
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Configure multer for file uploads
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname)); // Appending extension
+    }
+  });
+
+  const upload = multer({ storage: storage });
+
 // Create a transporter using SMTP
 const transporter = nodemailer.createTransport({
-    host: 'smtp.name', // Replace with your SMTP server address
-    port: 450,
+    host: 'smtpout', // Replace with your SMTP server address
+    port: 550,
     secure: true, // true for 465, false for other ports
     auth: {
-        user: 'email@example.com', // Replace with your SMTP username
-        pass: 'password' // Replace with your SMTP password
+        user: 'example@gmail.com', // Replace with your SMTP username
+        pass: 'example' // Replace with your SMTP password
     },
     logger: true, // Enable logging
     debug: true // Set logging level to "debug"
@@ -35,18 +55,25 @@ transporter.on('connect', () => {
 });
 
 // Route for handling form submission
-app.post('/send-email', (req, res) => {
+app.post('/send-email',upload.single('resume'), (req, res) => {
     // Extract form data
-    const { name, email, message } = req.body;
-
+    const { name, email, message} = req.body;
+    const resumePath = req.file.path;
     // Email options
     const mailOptions = {
-        from: email,
-        to: 'example@gmail.com',
+        from: 'example@gmail.com',
+        to: 'our@gmail.com',
         subject: `New message from ${name}`,
         html: `<p>Name:${name}</p>
         <p>${email}</p>
-        <p>Message: ${message}</p>`
+        
+        <p>Message: ${message}</p>`,
+        attachments: [
+            {
+              filename: req.file.originalname,
+              path: resumePath
+            }
+          ]
     };
 
     // Send email
@@ -56,14 +83,14 @@ app.post('/send-email', (req, res) => {
             res.status(500).send('Something went wrong. Please try again later.');
         } else {
             console.log('Email has been sent successfully.', info.response);
-            res.status(200).send('Email has been sent successfully.');
+            res.status(200).redirect('thank-you.html');
         }
     });
 });
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-    res.redirect('/index.html');  // Redirect to 'index.html' in the 'public' directory
+    res.redirect('index.html');  // Redirect to 'index.html' in the 'public' directory
   });
 
 // Start the server
